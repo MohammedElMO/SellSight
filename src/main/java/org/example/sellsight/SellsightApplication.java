fbe33e3096bef6db377d58d8c5b905d93e18fcef
MohammedElMO <amranimohamad040@gmail.com>
2026-04-13 15:13:06 +0100

    initalize spring boot

---
+++ src/main/java/org/example/sellsight/SellsightApplication.java
@@ -0,0 +1,13 @@
+package org.example.sellsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellsightApplication.class, args);
+	}
+
+}
